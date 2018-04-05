5d87e1e6479abb1d04ba10b0b3f0706572214f35
John Hawley <johnh@shotzoom.com>
2018-04-05 12:10:10 -0700

    Updated for latest swift compiler. Requires exhaustive switch cases

--- ARKit+CoreLocation/Source/SceneLocationView.swift
+++ ARKit+CoreLocation/Source/SceneLocationView.swift
@@ -489,6 +489,8 @@ open class SceneLocationView: ARSCNView, ARSCNViewDelegate {
             print("camera did change tracking state: normal")
         case .notAvailable:
             print("camera did change tracking state: not available")
+        case .limited(.relocalizing):
+            print("camera relocalizing")
         }
     }
 }
